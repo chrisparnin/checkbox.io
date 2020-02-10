00b39fe6a116c6c8158e685addd27a052500c91d
Chris Parnin <chris.parnin@gmail.com>
2020-02-10 11:03:34 -0500

    fix: use dirname to pick up path.

--- server-side/site/test/render.js
+++ server-side/site/test/render.js
@@ -1,11 +1,12 @@
 const marqdown = require('../marqdown');
 const fs = require('fs');
+const path = require('path');
 
 
 describe('Render markdown', function() {
     it('successfully renders survey', function(done) {
 
-        fs.readFile('resources/survey.md', function(err, data)
+        fs.readFile( path.join(__dirname, 'resources', 'survey.md'), function(err, data)
         {
             if( err ) throw err;
             let html = marqdown.render(data.toString());
