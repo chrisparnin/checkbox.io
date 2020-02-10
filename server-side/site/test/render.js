58bd396cfcc7293e6595069e14ceb7994adaf6b2
Chris Parnin <chris.parnin@gmail.com>
2020-02-10 10:59:41 -0500

    throw error if any. call done();

--- server-side/site/test/render.js
+++ server-side/site/test/render.js
@@ -7,9 +7,12 @@ describe('Render markdown', function() {
 
         fs.readFile('resources/survey.md', function(err, data)
         {
+            if( err ) throw err;
             let html = marqdown.render(data.toString());
 
             assert(html.includes("thead"));
+
+            done();
         });
 
     });
